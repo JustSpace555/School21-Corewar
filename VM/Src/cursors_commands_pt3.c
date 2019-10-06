22f4e4d2d69061620b755b69ee6839957c2a1793
Quinton Mebble <qmebble@mi-p1.21-school.ru>
2019-10-06 20:24:11 +0300

    move fix

--- VM/Src/cursors_commands_pt3.c
+++ VM/Src/cursors_commands_pt3.c
@@ -49,23 +49,11 @@ void	lld(t_cursor *cursor)
 	f_arg = get_first_arg(cursor, codage, 4, &offset);
 	s_arg = get_second_arg(cursor, codage, 4, &offset);
 	if (check_reg(s_arg))
+	{
 		cursor->reg[s_arg - 1] = f_arg;
-/*	if (codage >= 144 && codage <= 159)
-	{
-		CHECK_REG(&cursor, GET_CUR_POS_BYTE(&cursor, 6), 4, 1, 2);
-		value = get_int_data(cursor->cur_pos + 2);
-		reg = GET_CUR_POS_BYTE(&cursor, 6);
-		cursor->reg[reg - 1] = value;
+		if (g_vm->ver == 1)
+			ft_printf("P %4d | lld %d r%d\n", cursor->cursror_id, f_arg, s_arg);
 	}
-	else
-	{
-		CHECK_REG(&cursor, GET_CUR_POS_BYTE(&cursor, 4), 4, 1, 2);
-		value = get_int_data(arena_truncation(cursor->cur_pos + get_short_data(cursor->cur_pos + 2)));
-		cursor->reg[reg - 1] = value;
-	}
-	if (g_vm->ver == 1)
-		ft_printf("P %4d | lld %d r%d\n", cursor->cursror_id, value, reg);
-*/
 	move_cursor(cursor, 4, codage, 2);
 }
 
@@ -99,55 +87,6 @@ void	lldi(t_cursor *cursor)
 		else
 			cursor->reg[t_arg - 1] = get_int_data(f_arg + s_arg);
 	}
-/*	if ((codage & 0xC0) == 0x40)
-	{
-		CHECK_REG(&cursor, GET_CUR_POS_BYTE(&cursor, 2), 2, 1, 3);
-		if ((codage & 0x30) == 0x10)
-		{
-			CHECK_REG(&cursor, GET_CUR_POS_BYTE(&cursor, 3), 2, 1, 3);
-			dest_reg = GET_CUR_POS_BYTE(&cursor, 4);
-			CHECK_REG(&cursor, dest_reg, 2, 1, 3);
-			cursor->reg[dest_reg - 1] = get_int_data((
-				cursor->reg[GET_CUR_POS_BYTE(&cursor, 2) - 1] +
-				cursor->reg[GET_CUR_POS_BYTE(&cursor, 3) - 1]));
-		}
-		else if ((codage & 0x30) == 0x20)
-		{
-			dest_reg = GET_CUR_POS_BYTE(&cursor, 5);
-			CHECK_REG(&cursor, dest_reg, 2, 1, 3);
-			cursor->reg[dest_reg - 1] = get_int_data((
-				cursor->reg[GET_CUR_POS_BYTE(&cursor, 2) - 1] +
-				get_short_data(cursor->cur_pos + 3)));
-		}
-	}
-	else if ((codage & 0xC0) == 0x80)
-	{
-		if ((codage & 0x30) == 0x10)
-		{
-			CHECK_REG(&cursor, GET_CUR_POS_BYTE(&cursor, 4), 2, 1, 3);
-			dest_reg = GET_CUR_POS_BYTE(&cursor, 5);
-			CHECK_REG(&cursor, dest_reg, 2, 1, 3);
-			cursor->reg[dest_reg - 1] = get_int_data((
-				get_short_data(cursor->cur_pos + 2) +
-				cursor->reg[GET_CUR_POS_BYTE(&cursor, 5) - 1]));
-		}
-		else if ((codage & 0x30) == 0x20)
-		{
-			dest_reg = GET_CUR_POS_BYTE(&cursor, 6);
-			CHECK_REG(&cursor, dest_reg, 2, 1, 3);
-			cursor->reg[dest_reg - 1] = get_int_data((
-				get_short_data(cursor->cur_pos + 2) +
-				get_short_data(cursor->cur_pos + 4)));
-		}
-	}
-	else if ((codage & 0xC0) == 0xC0)
-	{
-		dest_reg = GET_CUR_POS_BYTE(&cursor, 6);
-		CHECK_REG(&cursor, dest_reg, 2, 1, 3);
-		cursor->reg[dest_reg - 1] = get_int_data(
-			get_short_data(cursor->cur_pos + 2) % IDX_MOD);
-	}
-*/
 	move_cursor(cursor, 2, codage, 3);
 }
 
