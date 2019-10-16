2bbc179cfb29440eaa31b1cd0078b43d70967f02
Anastasia Crooks <acrooks@at-p9.21-school.ru>
2019-10-16 12:10:42 +0300

    123

--- visualisator/script.bash
+++ visualisator/script.bash
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ ! -e ~/Library/Frameworks ]
+if ! [ -d /Library/Frameworks ]
 then
 	echo "Make folder Frameworks";
-	mkdir ~/Library Frameworks;
+	mkdir /Library/ Frameworks;
 fi;
-if [ ! -e ~/Library/Frameworks/SDL2.framework ]
+if ! [ -e /Library/Frameworks/SDL2.framework ]
 then
-	echo "Putting SDL2.framework in ~/Library/Frameworks";
-	cp -R SDL2.framework ~/Library/Frameworks/;
+	echo "Putting SDL2.framework in /Library/Frameworks";
+	cp -R SDL2.framework /Library/Frameworks/;
 fi;
-if [ ! -e ~/Library/Frameworks/SDL2_ttf.framework ]
+if ! [ -e /Library/Frameworks/SDL2_ttf.framework ]
 then
-	echo "Putting SDL2_ttf.framework in ~/Library/Frameworks";
-	cp -R SDL2_ttf.framework ~/Library/Frameworks/;
+	echo "Putting SDL2_ttf.framework in /Library/Frameworks";
+	cp -R SDL2_ttf.framework /Library/Frameworks/;
 fi;
