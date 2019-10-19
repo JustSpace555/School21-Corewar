79a6d8e1e50e60268db670e12a5f7a505faab574
Quinton Mebble <qmebble@oa-c4.21-school.ru>
2019-10-19 23:56:23 +0300

    script fixed

--- Vizualizator/script.bash
+++ Vizualizator/script.bash
@@ -8,10 +8,10 @@ fi;
 if ! [ -e /Users/$(whoami)/Library/Frameworks/SDL2.framework ]
 then
 	echo "Putting SDL2.framework in ~/Library/Frameworks";
-	cp -R $(pwd)/../Visualisator/SDL2.framework /Users/$(whoami)/Library/Frameworks/;
+	cp -R $(pwd)/../Vizualizator/SDL2.framework /Users/$(whoami)/Library/Frameworks/;
 fi;
 if ! [ -e /Users/$(whoami)/Library/Frameworks/SDL2_ttf.framework ]
 then
 	echo "Putting SDL2_ttf.framework in ~/Library/Frameworks";
-	cp -R $(pwd)/../Visualisator/SDL2_ttf.framework /Users/$(whoami)/Library/Frameworks/;
+	cp -R $(pwd)/../Vizualizator/SDL2_ttf.framework /Users/$(whoami)/Library/Frameworks/;
 fi;
